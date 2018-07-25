81a2beae9943ef88708ae2fe5f24cc904ee1d099
Anwesh43 <anweshthecool0@gmail.com>
2018-07-25 10:55:49 +0530

    adding Scheduler class

--- src/main/java/com/anwesh/impl/Scheduler.java
+++ src/main/java/com/anwesh/impl/Scheduler.java
@@ -1,4 +1,30 @@
 package com.anwesh.impl;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class Scheduler {
+
+    private static Scheduler scheduler = new Scheduler();
+
+    private ConcurrentLinkedQueue<SchedulerItem> schedulerItems = new ConcurrentLinkedQueue<>();
+
+    private Scheduler() {
+        start();
+    }
+
+    public static Scheduler getInstance() {
+        return scheduler;
+    }
+
+    public void schedule(Executable executable, long delay) {
+        schedulerItems.add(new SchedulerItem(executable, delay));
+    }
+
+    private void start() {
+        new Thread(() -> {
+            while(true) {
+                schedulerItems.stream().filter(item -> item.shouldExecute()).forEach(item -> item.shouldExecute());
+            }
+        }).start();
+    }
 }
