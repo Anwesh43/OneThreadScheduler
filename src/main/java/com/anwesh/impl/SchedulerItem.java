d4c87ff440c78c942c3b8fde41ea29e5ca0c241d
Anwesh43 <anweshthecool0@gmail.com>
2018-07-25 10:44:06 +0530

    adding SchedulerItem

---
+++ src/main/java/com/anwesh/impl/SchedulerItem.java
@@ -0,0 +1,32 @@
+package com.anwesh.impl;
+
+import java.util.function.Function;
+
+public class SchedulerItem {
+
+    private long delay;
+
+    private Executable task;
+
+    private long currTime;
+
+    public SchedulerItem(Executable task, long delay) {
+        this.task = task;
+        this.delay = delay;
+        currTime = System.currentTimeMillis();
+    }
+
+    public void execute() {
+        task.execute();
+        currTime = System.currentTimeMillis();
+    }
+
+    public boolean shouldExecute() {
+        return (System.currentTimeMillis() - currTime) >= delay;
+    }
+
+    @FunctionalInterface
+    private interface Executable {
+        void execute();
+    }
+}
