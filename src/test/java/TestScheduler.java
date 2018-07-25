a0722056ad41b0f8da0127af4e65ea7e30eb995b
Anwesh43 <anweshthecool0@gmail.com>
2018-07-26 00:58:19 +0530

    adding TestScheduler

--- src/test/java/TestScheduler.java
+++ src/test/java/TestScheduler.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import com.anwesh.impl.Scheduler;
 
 public class TestScheduler {
+
+    public static void main(String args[]) {
+        Scheduler scheduler = Scheduler.getInstance();
+        for(int i = 0; i < 4; i++) {
+            final String text = "hello " + i;
+            scheduler.schedule(() ->{
+                System.out.println(text);
+            }, (i + 1) * 1000);
+        }
+    }
 }
